6cc6c218bd1ba97ecbbc11b3e19c7a5bdd77bb08
rachel <rachel@seedsgives.com>
2023-04-16 16:42:30 -0400

    Added actual firebase config info, better organized scripts.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,86 +1,20 @@
-// // Add all product data to Firebase (images, price, title, etc).
 
-// image:
-// price:
-// title:
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// //display the number of items currently added to the user's cart
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBv__kBiOmoOFs1738i2iG5ybpV7RNmBVU",
+    authDomain: "sheh-rachel-project-two.firebaseapp.com",
+    projectId: "sheh-rachel-project-two",
+    storageBucket: "sheh-rachel-project-two.appspot.com",
+    messagingSenderId: "246534187893",
+    appId: "1:246534187893:web:e121f9caddb5548544413c"
+  };
 
-// set numbers of items current addred to the user's cart
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
-// // If we hit stretch goal we can then refactor data to include:
-// // Remove an item from the cart.
-
-// remove a product object from the cart
-
-// // See the total price of all items.
-
-
-
-// //Implement a search bar that allows users to search for products by name.
-
-// search returns the product object of every product matching the search query
-
-{ "data": [
-    {
-      title: "Malm Chair",
-      url: "images/bowie.jpg",
-      price: 22,
-      stock: 4,
-    },
-    {
-      title: 'Pendant Lamp',
-      url: 'images/doughnut.jpg',
-      price: 45.00,
-      stock: 5,
-    },
-    {
-      title: 'Magnolia Dream',
-      url: 'images/journey.jpg',
-      price: 18.00,
-      stock: 6
-    },
-    {
-      title: 'Malm Chair',
-      url: 'images/someurl.jpg',
-      price: 45.00,
-      stock: 0
-    },
-    {
-      title: 'Pendant Lamp',
-      url: 'images/skeleton.jpg',
-      price: 45.00,
-      stock: 10
-    },
-    {
-      title: 'Candle',
-      price: 45.00,
-      stock: 4
-    }
-    {
-      title: 'Lorem Ipsum',
-      price: 45.00,
-      stock: 4
-    }
-    {
-      title: 'Pendant Lamp',
-      price: 45.00,
-      stock: 4
-    }
-    {
-      title: 'Pendant Lamp',
-      price: 45.00,
-      stock: 4
-    }
-    {
-      title: 'Pendant Lamp',
-      price: 45.00,
-      stock: 4
-    }
-    {
-      title: 'Pendant Lamp',
-      price: 45.00,
-      stock: 4
-    }
-  ]
-}
+  export default app;
--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+
+import app from "./firebase-config.js";
+import {getDatabase, ref, set, onValue} from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+
+const database = getDatabase(app);
+const dbRef = ref(database);
+
 //PSEUDOCODE DRAFT FOR PROJECT TWO
 
 
@@ -25,6 +32,7 @@
   // ^ that would transition the user to a separate page with search bar + results
 // Add event listeners to "add to cart" buttons
 // Connect shopping cart image to event listener so that it counts each time the user clicks on "add to cart"
+
 
 //RACHEL'S DRAFT
 
@@ -57,64 +65,154 @@
         // Use Firebase's update() function to send the updated list of Pokemon to Firebase (this will trigger the onValue() listener to update the page).
 
 
+
+        //DATA STRUCTURE FOR FIREBASE (PROJECT TWO)
+
+// // Add all product data to Firebase (images, price, title, etc).
+
+// image:
+// price:
+// title:
+
+// //display the number of items currently added to the user's cart
+
+// set numbers of items current addred to the user's cart
+
+// // If we hit stretch goal we can then refactor data to include:
+// // Remove an item from the cart.
+
+// remove a product object from the cart
+
+// // See the total price of all items.
+
+
+
+// //Implement a search bar that allows users to search for products by name.
+
+// search returns the product object of every product matching the search query
+
+// { "data": [
+//   {
+//     title: "Malm Chair",
+//     url: "images/bowie.jpg",
+//     price: 22,
+//     stock: 4,
+//   },
+//   {
+//     title: 'Pendant Lamp',
+//     url: 'images/doughnut.jpg',
+//     price: 45.00,
+//     stock: 5,
+//   },
+//   {
+//     title: 'Magnolia Dream',
+//     url: 'images/journey.jpg',
+//     price: 18.00,
+//     stock: 6
+//   },
+//   {
+//     title: 'Malm Chair',
+//     url: 'images/someurl.jpg',
+//     price: 45.00,
+//     stock: 0
+//   },
+//   {
+//     title: 'Pendant Lamp',
+//     url: 'images/skeleton.jpg',
+//     price: 45.00,
+//     stock: 10
+//   },
+//   {
+//     title: 'Candle',
+//     price: 45.00,
+//     stock: 4
+//   }
+//   {
+//     title: 'Lorem Ipsum',
+//     price: 45.00,
+//     stock: 4
+//   }
+//   {
+//     title: 'Pendant Lamp',
+//     price: 45.00,
+//     stock: 4
+//   }
+//   {
+//     title: 'Pendant Lamp',
+//     price: 45.00,
+//     stock: 4
+//   }
+//   {
+//     title: 'Pendant Lamp',
+//     price: 45.00,
+//     stock: 4
+//   }
+//   {
+//     title: 'Pendant Lamp',
+//     price: 45.00,
+//     stock: 4
+//   }
+// ]
+// }
+
+
 //ADDING JS FUNCTIONALIY FOR ASSIGNMENT #2
-        //find the form element on the page
-const formElement = document.querySelector("form");
+        // //find the form element on the page
+        // const formElement = document.querySelector("form");
 
-formElement.addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    const inputElement = document.querySelector("input");
-    const task = inputElement.value;
+        // formElement.addEventListener("submit", function(event) {
+        //     event.preventDefault();
+            
+        //     const inputElement = document.querySelector("input");
+        //     const task = inputElement.value;
+        
+        // });
+        
+        //trying to figure out some error handling for form submissions.  
+        
+            //if(task !== ""){  
+            //console.log(task);
+            //} else {
+              //pop a message that says "Please complete the form before submitting"?
+            //} 
+        
+        //Making mobile hamburger menu functional
+        
+        // const menu = document.querySelector(".menu");
+        // const hamburger = document.querySelector(".hamburgerMenu");
+        // const open = document.querySelector(".fa-solid.fa.bars");
+        // const close = document.querySelector(".fa-solid.fa-xmark"); 
+        
+        // hamburger.addEventListener("click", function() {
+        //   // Open menu
+        //   if (!menu.classList.add("showMenu")) {
+        //     menu.classList.add("showMenu");
+        //     open.style.display = "none";
+        //     close.style.display = "block";
+        //     close.style.fontSize = "2rem";
+        //     close.style.paddingTop = "10px";
+        //     menu.style.display = "block";
+        //   }
+        //   // Close menu
+        //   else {
+        //     menu.classList.remove("showMenu");
+        //     open.style.display = "block";
+        //     close.style.display = "none";
+        //     menu.style.display = "flex";
+        //   }
+        // });
+        
+        // const navUl = document.querySelector("nav.ul");
+        // let clicked = false;
+        
+        // const hamburger = document.querySelector(".hamburger-label"); 
+        // hamburger.addEventListener("click", function(){
+        //     if (clicked===false){
+        //       navUl.style.display="block"
+        //     }
+        // });
+        
+        //at say 960px screen sizes and smaller, the hamburger menu should appear per media query, and the nav links should disappear and flex-direction: column when you click on the hamburger menu.  What do I need to use JS for to make them more "functional" than they are now, since they currently link to the correct pages?  Is it not possible to do that within a hamburger menu using HTML?
 
-});
-
-//trying to figure out some error handling for form submissions.  
-
-    //if(task !== ""){  
-    //console.log(task);
-    //} else {
-      //pop a message that says "Please complete the form before submitting"?
-    //}
 
 
-//Adding a pop-up modal (on page load, or for images, etc)
-
-
-//Making mobile hamburger menu functional
-
-const menu = document.querySelector(".menu");
-const hamburger = document.querySelector(".hamburgerMenu");
-const open = document.querySelector(".fa-solid.fa.bars");
-const close = document.querySelector(".fa-solid.fa-xmark"); 
-
-hamburger.addEventListener("click", function() {
-  // Open menu
-  if (!menu.classList.add("showMenu")) {
-    menu.classList.add("showMenu");
-    open.style.display = "none";
-    close.style.display = "block";
-    close.style.fontSize = "2rem";
-    close.style.paddingTop = "10px";
-    menu.style.display = "block";
-  }
-  // Close menu
-  else {
-    menu.classList.remove("showMenu");
-    open.style.display = "block";
-    close.style.display = "none";
-    menu.style.display = "flex";
-  }
-});
-
-// const navUl = document.querySelector("nav.ul");
-// let clicked = false;
-
-// const hamburger = document.querySelector(".hamburger-label"); 
-// hamburger.addEventListener("click", function(){
-//     if (clicked===false){
-//       navUl.style.display="block"
-//     }
-// });
-
-//at say 960px screen sizes and smaller, the hamburger menu should appear per media query, and the nav links should disappear and flex-direction: column when you click on the hamburger menu.  What do I need to use JS for to make them more "functional" than they are now, since they currently link to the correct pages?  Is it not possible to do that within a hamburger menu using HTML?
