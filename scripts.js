fad68c8533a03d45628216c15595d02b4f808fc5
shehsarfraz <anushehsarfraz@gmail.com>
2023-04-24 10:55:18 -0400

    search bar completed

--- scripts.js
+++ scripts.js
@@ -253,68 +253,69 @@ const addToDatabase = (key, value) => {
 
   icons.addEventListener('click', toggleSearch);
 
-
     // created event listener that assigns the button to the variable of submit
     const submit = document.querySelector('.submit')
+
     // function that says - once the submit button is pushed on click then to then grab the value inside the element that has an id of 'search' (the search bar)
     submit.addEventListener('click', (event) => {
       // this prevents the default state of refreshing on click
+
       event.preventDefault();
       // this selects the value of what is entered in the search bar and is assigned to userInput variable 
-      const userInput = document.getElementById('search').value;
+      let userInput = document.getElementById('search').value;
       searchDatabase(userInput)
-
-      
+      if (document.getElementById('search').value) {
+        document.getElementById('search').value = '';
+        userInput = '';
+      }
     });
     // here we are passing the argument of userInput to the searchDatabase function by calling it above and creating the function that will filter through our results to find a matching title in our database
     const searchDatabase = (userInput) => {
       // line that references our database
-      onValue(dbRef, function(data) {
-        // adding a variable for the data value in our database
-        const ourData = data.val();
-        // assigned a variable to the specific array in our database
-        const inventory = ourData.inventory;
-        // filter that sorts through the inventory array and returns search results that match the item title based on the user input ALSO turns input into lowercase on return so that capitalization does not matter and removes any white space at the end of userinput using trim()
-          const searchResults = inventory.filter((item) => {
-            return item.title.includes(userInput.toLowerCase().trim())
-      
+      if (userInput) {
+        onValue(dbRef, function(data) {
+          // adding a variable for the data value in our database
+          const ourData = data.val();
+          // assigned a variable to the specific array in our database
+          const inventory = ourData.inventory;
+          // filter that sorts through the inventory array and returns search results that match the item title based on the user input ALSO turns input into lowercase on return so that capitalization does not matter and removes any white space at the end of userinput using trim()
+            let searchResults = inventory.filter((item) => {
+              return item.title.includes(userInput.toLowerCase().trim())
+        
+          })
+          
+          // by passing the argument of search results by calling the function above, we can move the info from search results down to this function
+          addSearchResults(searchResults)
+          searchResults = [];
         })
-        
-        // by passing the argument of search results by calling the function above, we can move the info from search results down to this function
-        addSearchResults(searchResults)
-        
-      })
+      }
+      else {
+        alert('Please enter a search query.');
+      }
     }
     // creating a function for adding search results to the page and passing it above to the function that filters and returns userinput. 
     const addSearchResults = (searchResults) => {
-    searchResults.forEach((object) => {
-      // add a line of code that empties the ul every time to avoid duplication of results
-
-      // creating html elements so that we can append them to the ul 
-      const listItem = document.createElement('li');
-      const image = document.createElement('img');
-      const title = document.createElement('p');
-      const price = document.createElement('p');
-      // selecting each element and assigning it to the key value pair in the array on firebase
-      image.src = object.url
-      title.innerText = object.title
-      price.innerText = object.price
-      // appending those items to the li so that we can display them in the empty UL 
-      listItem.append(image, title, price);
-      document.querySelector('#results').append(listItem)
+      document.querySelector('#results').innerHTML = ''; 
+      searchResults.forEach((object) => {
+        // add a line of code that empties the ul every time to avoid duplication of results
+        // creating html elements so that we can append them to the ul 
+        const listItem = document.createElement('li');
+        const image = document.createElement('img');
+        const title = document.createElement('p');
+        const price = document.createElement('p');
+        const alt = document.createElement('alt')
+        // selecting each element and assigning it to the key value pair in the array on firebase
+        image.src = object.url
+        title.innerText = object.title
+        price.innerText = object.price
+        image.alt = object.alt 
+        // appending those items to the li so that we can display them in the empty UL 
+        listItem.append(image, title, price, alt);
+        document.querySelector('#results').append(listItem)
       })
     }
 
 
-    // TO DO: 
-    // Empty search bar after entering search results 
-    // Empty container before reloading with new results upon submit 
-    // possibly this code could be useful?       
-      // const ulElement = document.querySelector('#results');
-      // ulElement = '';
-    // UNSURE where to plug it in though - hasn't worked anywhere I thought to put it
-    // Make sure mobile version is readable 
-    // IF HAVE TIME - error handling, put in message that says "no results found"
 
 
 
